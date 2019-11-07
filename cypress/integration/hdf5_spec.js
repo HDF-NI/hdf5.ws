8648410f2ffc3afa57513643db28dca67ad05339
rimmartin <mathmldashx@yahoo.com>
2019-11-07 06:37:28 -0500

    Update hdf5_spec.js

--- cypress/integration/hdf5_spec.js
+++ cypress/integration/hdf5_spec.js
@@ -3,7 +3,7 @@ describe('HDF5 Interface Test', function() {
       const dataTransfer = new DataTransfer;
     cy.server()
     cy.route('POST', '/read_image/').as('ri')
-    cy.visit('http://roger-G74Sx:8888/images.html')
+    cy.visit('http://localhost:8888/images.html')
     cy.get('#nwCanvas').trigger('mousedown', {which: 1})
                        .trigger('dragstart', {dataTransfer}).trigger('drag', {dataTransfer}).trigger('dragend')
                        .trigger('mousemove', {clientX: 250, clientY: 381 })
